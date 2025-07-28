c370621af3456d5fd3325e5a9085fdd0332cf17d
Logan <LoganHaug>
2025-07-27 23:11:35 -0700

    Add dualshock 4 starter control code

--- crab.py
+++ crab.py
@@ -1,16 +1,34 @@
 import bluetooth
+import pygame
 
 import time
 
-esp_mac = "1C:69:20:94:6E:1E"
+pygame.init()
 
+def exit():
+    sock.close()
+
+with open("mac.txt", "r") as f:
+    esp_mac = f.readline().strip() 
 try:
     sock = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
     sock.connect((esp_mac, 1))
     time.sleep(2)
-    sock.send("test")
-    data = sock.recv(1024)
-    print(type(data))
 except bluetooth.btcommon.BluetoothError as e:
     print(f"Error: {e}")
-sock.close()
+
+
+controller = None
+while True:
+    for event in pygame.event.get():
+        print(event)
+        if event.type == pygame.QUIT:
+            exit()
+        if event.type == pygame.JOYDEVICEADDED:
+            controller = pygame.joystick.Joystick(event.device_index)
+        if event.type == pygame.JOYDEVICEREMOVED:
+            controller = None
+        if controller is not None:
+            print(controller.get_instance_id())
+            # TODO movement code: horse or crab mode, raise / lower, joystick movement (left stick), turn (right stick)
+
